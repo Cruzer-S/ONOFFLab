c5fe913fea0647f0a3a2a2bfdeb7644e402e86a2
Cruzer-S <yyyynoom@gmail.com>
2021-02-16 17:07:26 +0900

    add many things

--- raspberry-4/main.c
+++ raspberry-4/main.c
@@ -9,169 +9,7 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
-#define BROADCAST_ID 0xFE
-
-#define PFX_COMMAND(PFX)		\
-	enum command {				\
-		PFX##EEP_WRITE = 0x01,	\
-		PFX##EEP_READ,			\
-		PFX##RAM_READ,			\
-		PFX##RAM_WRITE,			\
-		PFX##I_JOG,				\
-		PFX##S_JOG,				\
-		PFX##STAT,				\
-		PFX##ROLLBACK,			\
-		PFX##REBOOT				\
-	};
-
-#define NORMAL_COMMAND(VOID)	\
-	enum command {				\
-		EEP_WRITE = 0x01,		\
-		EEP_READ,				\
-		RAM_READ,				\
-		RAM_WRITE,				\
-		I_JOG,					\
-		S_JOG,					\
-		STAT,					\
-		ROLLBACK,				\
-		REBOOT					\
-	};
-
-#define PFX_RAM_REGISTER(PFX)	\
-	enum ram_register_map {		\
-	PFX##ID	=	0,				\
-	PFX##ACK_POLICY,			\
-	PFX##ALARM_LED_POLICY,		\
-	PFX##TORQUE_POLICY,			\
-	PFX##MAX_TEMPERATURE = 5,	\
-	PFX##MIN_VOLTAGE,			\
-	PFX##MAX_VOLTAGE,			\
-	PFX##ACCELERATION_RATIO,	\
-	PFX##MAX_ACCELERATION,		\
-	PFX##DEAD_ZONE,				\
-	PFX##SATURATIOR_OFFSET,		\
-	PFX##SATURATOOR_SLOPE,		\
-	PFX##PWM_OFFSET,			\
-	PFX##MIN_PWM,				\
-	PFX##MAX_PWM,				\
-	PFX##PWM_THRESHOLD,			\
-	PFX##MIN_POSITION,			\
-	PFX##MAX_POSITION,			\
-	PFX##POSITION_KP,			\
-	PFX##POSITION_KD,			\
-	PFX##POSITION_KI,			\
-	PFX##POSITION_FEED_FORWARD1,\
-	PFX##POSITION_FEED_FORWARD2,\
-	PFX##LED_BLINK_PERIOD = 38,	\
-	PFX##ADC_FAULT_PERIOD,		\
-	PFX##PACKET_GARBAGE_PERIOD,	\
-	PFX##STOP_DETECTION_PERIOD,	\
-	PFX##OVLD_DETECTION_PERIOD,	\
-	PFX##STOP_THRESHOLD,		\
-	PFX##INPOSITION_MARGIN,		\
-	PFX##CAL_DIFFERENCE = 47,	\
-	PFX##STATUS_ERROR,			\
-	PFX##STATUS_DETAIL,			\
-	PFX##TORQUE_CONTROL = 52,	\
-	PFX##LED_CONTROL,			\
-	PFX##VOLTAGE,				\
-	PFX##TEMPERATURE,			\
-	PFX##CURRENT_CONTROL_MODE,	\
-	PFX##TICK,					\
-	PFX##CAL_POSITION,			\
-	PFX##ABS_POSITION = 60,		\
-	PFX##DIF_POSITION = 62,		\
-	PFX##PWM = 64,				\
-	PFX##ABS_GOAL_POSITION = 68,\
-	PFX##ABS_DSIR_POSITION = 70,\
-	PFX##DESIRED_VELOCITY = 72	\
-};
-
-#define PFX_NVT_REGISTER(PFX)		\
-	enum volatile_register {		\
-		PFX##MODEL_NO1 = 1,			\
-		PFX##MODEL_NO2,				\
-		PFX##VERSION_NO1,			\
-		PFX##VERSION_NO2,			\
-		PFX##BAUD_RATE,				\
-		PFX##ID = 7,				\
-		PFX##ACK_POLICY,			\
-		PFX##ALARM_LED_POLICY,		\
-		PFX##TORQUE_POLICY,			\
-		PFX##MAX_TEMPERATURE = 12,	\
-		PFX##MIN_VOLTAGE,			\
-		PFX##MAX_VOLTAGE,			\
-		PFX##ACCELERATION_RATIO,	\
-		PFX##DEAD_ZONE,				\
-		PFX##SATURATIOR_OFFSET,		\
-		PFX##SATURATIOR_SLOPE,		\
-		PFX##PWM_OFFSET,			\
-		PFX##MIN_PWM,				\
-		PFX##MAX_PWM,				\
-		PFX##OVERLOAD_PWM_THRESHOLD,\
-		PFX##MIN_POSITON,			\
-		PFX##MAX_POSTION,			\
-		PFX##POSITION_KP,			\
-		PFX##POSITION_KI,			\
-		PFX##POSITION_FEED_FORWARD1,\
-		PFX##POSITIOO_FEED_FORWARD2,\
-		PFX##LED_BLINK_PERIOD = 33,	\
-		PFX##ADC_FAULT_CHECK_PERIOD,\
-		PFX##PACKET_GARBAGE_CHECK,	\
-		PFX##STOP_DETECTION_PERIOD,	\
-		PFX##OVLD_DETECTION_PERIOD,	\
-		PFX##STOP_THRESHOLD,		\
-		PFX##INPOSITION_MARGIN,		\
-		PFX##CAL_DIFFERENCE = 42,	\
-		PFX##STATUS_DIFFERENCE,		\
-		PFX##STATUS_ERROR,			\
-		PFX##STATUS_DETAIL,			\
-		PFX##TORQUE_CONTROL = 47,	\
-		PFX##LED_CONTROL,			\
-		PFX##VOLTAGE,				\
-		PFX##TEMPERATURE,			\
-		PFX##CURRENT_CONTROL_MODE,	\
-		PFX##TICK,					\
-		PFX##CAL_POSITION,			\
-		PFX##ABS_POSITION,			\
-		PFX##DESIRED_VELOCITY		\
-	};
-
-#define PFX_LED_COLOR(PFX)	\
-	enum color {			\
-		PFX##GREEN	= 0x01,	\
-		PFX##BLUE	= 0x02,	\
-		PFX##RED	= 0x04	\
-	};
-
-
-#define PFX_CONTROL_MODE(PFX)	\
-	enum control_mode {			\
-		PFX##POSITION,			\
-		PFX##VELECITY			\
-	};
-
-#define PFX_STATUS_ERROR(PFX)			\
-	enum status_error {					\
-		PFX##EXCEED_VOLTAGE		= 0x01,	\
-		PFX##EXCEED_POT			= 0x02,	\
-		PFX##EXCEED_TEMPERATURE = 0x04,	\
-		PFX##INVALID_PACKET		= 0x08,	\
-		PFX##OVLD_DETECTED		= 0x10,	\
-		PFX##DV_FAULT_DETECTED	= 0x20,	\
-		PFX##EEP_REG_DISTORTED	= 0x40	\
-	};
-
-#define PFX_STATUS_DETAIL(PFX)			\
-	enum status_detail {				\
-		PFX##MOVING_FLAG		= 0x01,	\
-		PFX##INPOSITION_FLAG	= 0x02,	\
-		PFX##CHECKSUM_FLAG		= 0x04,	\
-		PFX##UNKOWN_COMMAND		= 0x08,	\
-		PFX##EXCEED_REG_RANGE	= 0x10,	\
-		PFX##GARBAGE_DETECTED	= 0x20,	\
-		PFX##MONITOR_ON_FLAG	= 0x40	\
-	};
+#include "servo_hrx.h"
 
 PFX_COMMAND(HRX_CMD_);
 PFX_RAM_REGISTER(HRX_RAM_REG_);
@@ -197,6 +35,7 @@ struct motor {
 };
 
 _Noreturn void error_handling(const char *format, ...);
+struct motor hrx_create_motor(byte id);
 
 int main(int argc, char *argv[])
 {
@@ -317,7 +156,8 @@ void close_serial(int serial)
 	serialClose(serial);
 }
 
-void hrx_connect_serial(struct motor *motor, int serial)
+struct motor hrx_connect_serial(struct motor *motor, int serial)
 {
 	motor->serial = serial;
+	
 }
---
+++ raspberry-4/servo_hrx.h
@@ -0,0 +1,6 @@
+#ifndef _SERVO_HRX_H_
+#define _SERVO_HRX_H_
+
+#include "hrx_def.h"
+
+#endif
