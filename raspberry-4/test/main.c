046e5264ef33336a4e5071534e400f65b94f9640
Cruzer-S <yyyynoom@gmail.com>
2021-02-17 16:46:10 +0900

    change main.c

--- raspberry-4/test/main.c
+++ raspberry-4/test/main.c
@@ -30,9 +30,9 @@ int main(void)
 
 	while (true)
 	{
-		// scrape_serial(serial_port, DEBUG_DELAY, LINE_PER_BYTE, true);
+		scrape_serial(serial_port, DEBUG_DELAY, LINE_PER_BYTE, true);
 		serialPuts(serial_port, "hello, world! my name is yeounsu moon good to see you :)");
-		delayMicroseconds(2000);
+		delayMicroseconds(5000);
 	}
 
 	serialClose(serial_port);
