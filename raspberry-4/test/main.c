5560c4940d2812ee1917fa06fef0c668df0918d0
Cruzer-S <yyyynoom@gmail.com>
2021-02-17 10:56:40 +0900

    sdf

--- raspberry-4/test/main.c
+++ raspberry-4/test/main.c
@@ -7,28 +7,30 @@
 
 int main (int argc, char *argv[])
 {
-	int fd ;
+	int fd;
 
 	if ((fd = serialOpen ("/dev/ttyAMA0", 115200)) < 0)
 	{
-		fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+		fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno));
 		return 1 ;
 	}
 
+	printf("fd: %d \n", fd);
+
 	if (wiringPiSetup () == -1)
 	{
-		fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+		fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno));
 		return 1 ;
 	}
 
 	while (true) 
 	{
-		if (serialDataAvail(fd)) {
+		if (serialDataAvail(fd)) 
+		{
 			printf("<-");
 			while (serialDataAvail(fd))
-			{
 				printf ("%02x ", serialGetchar (fd));
-			}
+
 			printf("\n");
 			fflush (stdout) ;
 		}
