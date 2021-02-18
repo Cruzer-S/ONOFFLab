a6ac794f4166d35791df7b53f66234d61c5e60a3
Cruzer-S <yyyynoom@gmail.com>
2021-02-18 17:06:33 +0900

    change debugger.h

--- raspberry-4/test/debugger.h
+++ raspberry-4/test/debugger.h
@@ -4,6 +4,7 @@
 #include <stdarg.h>			// va_list, va_start, va_end
 #include <stdio.h>			// vfprintf
 #include <stdlib.h>			// exit, EXIT_FAILURE
+#include <stdbool.h>		// bool
 #include <wiringSerial.h>	// serialDataAvail, serialGetchar, delayMicroseconds
 
 _Noreturn void error_handling(const char *formatted, ...);
