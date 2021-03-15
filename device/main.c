dd3e06121cd6aa9de18a96cc96b078920a937131
Cruzer-S <yyyynoom@gmail.com>
2021-03-15 19:52:44 +0900

    auto commit and push

--- device/main.c
+++ device/main.c
@@ -211,7 +211,7 @@ int32_t wait_command(int sock)
 	int32_t command;
 	int ret;
 
-	if (sock < 0) return -1;
+	if (sock < 0) return -2;
 
 	if ((ret = recvt(sock, &command, sizeof(command), CPS)) < 0)
 		return (ret == -2) ? -1 : 0;
