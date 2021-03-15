f08867a75ba7165c804bc484300f97d7d46fa97f
Cruzer-S <yyyynoom@gmail.com>
2021-03-15 20:18:14 +0900

    auto commit and push

--- device/main.c
+++ device/main.c
@@ -97,9 +97,6 @@ int main(int argc, char *argv[])
 		// ========================================================================
 		int command;
 		if ((command = wait_command(serv_sock)) < 0) {
-			printf("command: %d \n", command);
-			printf("serv_sock: %d \n", serv_sock);
-
 			if (serv_sock > 0) {
 				printf("disconnect to target \n");
 				close(serv_sock);
