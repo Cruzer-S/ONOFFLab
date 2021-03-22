d908551de79bc69278284e986b7ba949a1fcde8b
Cruzer-S <yyyynoom@gmail.com>
2021-03-22 16:02:17 +0900

    auto commit and push

--- device/source/task_manager.c
+++ device/source/task_manager.c
@@ -37,11 +37,13 @@ void delete_task_manager(struct task_manager *tm)
 int make_task(struct task_manager *tm)
 {
 	if (tm->cur + 1 >= tm->max)
-		return -1;
+		return -2;
 
 	for (int i = 0; i < tm->cur + 1; i++)
 		if (!tm->task[i]) {
 			tm->task[i] = true;
+			if (tm->cur + 1 == i)
+				tm->cur++;
 			return i;
 		}
 
