37a369590d878027207cf5229344db70f8611ca5
Cruzer-S <yyyynoom@gmail.com>
2021-06-01 13:46:38 +0900

    union timer

--- union/source/logger.c
+++ union/source/logger.c
@@ -7,22 +7,9 @@
 
 char *get_time0(char *buf, size_t sz_buf)
 {
-#define STR_TIME_FORMAT "%H:%M:%S"
-	struct timespec tspec;
-	struct tm tm_now;
+	time_t current = time(NULL);
 
-	size_t sz_ret;
-
-	if (buf == NULL || sz_buf < 20)
-		return NULL;
-
-	if (clock_gettime(CLOCK_MONOTONIC, &tspec) == -1)
-		return NULL;
-
-	localtime_r((time_t *) &tspec.tv_sec, &tm_now);
-	if ((sz_ret = strftime(buf, sz_buf, STR_TIME_FORMAT, &tm_now)) == 0)
-		return NULL;
+	strftime(buf, sz_buf, "%b %d %H:%M:%S", localtime(&current));
 
 	return buf;
-#undef STR_TIME_FORMAT
 }
