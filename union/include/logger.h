ae2c50abeb8f61fb639f9a7fa1bdbeed0b64031e
Cruzer-S <yyyynoom@gmail.com>
2021-06-01 13:28:29 +0900

    change many things

--- union/include/logger.h
+++ union/include/logger.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>		// fprintf, stdout, stderr, size_t
 #include <stdlib.h>		// exit(), EXIT_FAILURE
+#include <stdbool.h>
 
 #ifdef REDIRECTION
 #define STDOUT REDIRECTION
@@ -14,13 +15,28 @@
 #define STDCRT stderr
 #endif
 
-#define print_message(FPTR, TYPE, FMT, ...)					\
-	fprintf(FPTR, "["#TYPE"] [%s/%s:%03d] " FMT "\n", 		\
-			__FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)	
+#define print_message(FPTR, TYPE, FMT, ...) do {				\
+	char buf[512];												\
+	get_time0(buf, sizeof(buf) - 1);							\
+	fprintf(FPTR, "[%s] ["#TYPE"] [%s/%s:%03d] " FMT "\n", 		\
+			buf, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);\
+} while (false)
+
+/*
+#define print_message(FPTR, TYPE, FMT, ...) do {				\
+	char buf[512];												\
+	get_time0(buf, sizeof(buf) - 1);							\
+	fprintf(FPTR, "[%s] ["#TYPE"] [%s/%s:%03d] " FMT "\n", 		\
+			buf, __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);\
+} while (false)
+*/
 
 #define pr_out(FMT, ...) print_message(STDOUT, INF, FMT, __VA_ARGS__)
 #define pr_err(FMT, ...) print_message(STDERR, ERR, FMT, __VA_ARGS__)
-#define pr_crt(FMT, ...) print_message(STDERR, CRI, FMT, __VA_ARGS__), exit(EXIT_FAILURE)
+#define pr_crt(FMT, ...) do { 						\
+	print_message(STDERR, CRI, FMT, __VA_ARGS__); 	\
+	exit(EXIT_FAILURE); 							\
+} while (false)
 
 #define GET_TIME0(X) (get_time0(X, sizeof(X)) == NULL ? "error" : X)
 
